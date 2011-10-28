351729b0faaa4892544fdaf7ec7c25f8a174e81b
Chris Utz <chris.utz@nextthought.com>
2011-10-28 03:33:54 +0000

    Move TextScrollView to NTIFoundation and rename it to be more descriptive

---
+++ NTIFoundation/RTFEditing.subproj/NTIEditableFrameScrollView.m
@@ -0,0 +1,95 @@
+// Copyright 2010 The Omni Group.  All rights reserved.
+//
+// This software may only be used and reproduced according to the
+// terms in the file OmniSourceLicense.html, which should be
+// distributed with this project and can also be found at
+// <http://www.omnigroup.com/developer/sourcecode/sourcelicense/>.
+
+#import "TextScrollView.h"
+
+#import <OmniUI/OUIEditableFrame.h>
+#import <OmniUI/OUIMinimalScrollNotifierImplementation.h>
+
+@interface NTITextScrollViewDelegate : OUIMinimalScrollNotifierImplementation<UIScrollViewDelegate>
+{
+	id _textView;
+}
+-(id)initWithView: (id)view;
+@end
+
+@implementation NTITextScrollViewDelegate
+
+-(id)initWithView: (id)view
+{
+	self = [super init];
+	self->_textView = view;
+	return self;
+}
+
+-(UIView*)viewForZoomingInScrollView: (id)_
+{
+	return self->_textView;
+}
+
+@end
+
+@implementation TextScrollView
+
+@synthesize textView = _textView;
+
+-(void)installDelegate
+{
+	if( self.delegate == nil && self->_textView ) {
+		//If the delegate is missing, scroll offsets can be wrong.
+		if( self->_scrollDelegate == nil ) {
+			self->_scrollDelegate = [[NTITextScrollViewDelegate alloc] initWithView: self->_textView];
+		}
+		self.delegate = self->_scrollDelegate;
+	}
+}
+
+-(id)initWithCoder: (NSCoder*)coder
+{
+	self = [super initWithCoder: coder];
+	[self installDelegate]; 
+	return self;
+}
+
+-(id)delegate
+{
+	id r = [super delegate];
+	if( !r && self->_textView ) {
+		//During the initialization process from a NIB, we need to fake one
+		//of these before we can actually install it.
+		r = [[NTITextScrollViewDelegate alloc] initWithView: self->_textView];
+	}
+	return r;
+}
+
+-(void)layoutSubviews;
+{
+	OBPRECONDITION(_textView); // not hooked up in xib?
+	
+	if( _textView ) {
+		[self installDelegate];
+		CGRect bounds = self.bounds;
+
+		// First make sure the text is the right width so that it can
+		//calculate the right used height
+		CGRect textFrame = _textView.frame;
+		if( textFrame.size.width != bounds.size.width ) {
+			_textView.frame = CGRectMake(0, 0, bounds.size.width, textFrame.size.height);
+		}
+
+		// Then ensure the height is large enough to span the text (or our height).
+		CGSize usedSize = _textView.viewUsedSize;
+		CGFloat height = MAX(bounds.size.height, usedSize.height);
+		if( height != textFrame.size.height ) {
+			_textView.frame = CGRectMake(0, 0, bounds.size.width, height);
+		}
+	}
+
+	[super layoutSubviews];
+}
+
+@end
