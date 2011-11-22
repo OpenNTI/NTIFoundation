c13bba759614248743d73721324c46b3cebf8aa8
Chris Utz <chris.utz@nextthought.com>
2011-11-22 02:19:09 +0000

    Add a couple of simple tests for object -> attributedString and attributedString -> object conversion.  Add a test that illustrates one of the current issues with attributed string chunking.  This test does not currenty pass.

--- UnitTests/NSAttributedString-NTIExtensionsTest.m
+++ UnitTests/NSAttributedString-NTIExtensionsTest.m
@@ -9,8 +9,101 @@
 #import "NSAttributedString-NTIExtensionsTest.h"
 #import "NSAttributedString-NTIExtensions.h"
 #import <OmniAppKit/OATextStorage.h>
+#import <OmniAppKit/OATextAttachment.h>
+#import <OmniAppKit/OATextAttachmentCell.h>
+
+@interface ObjectAttachmentCell : OATextAttachmentCell {
+@private
+    id object;
+}
+-(id)initWithObject: (id)obj;
+@property (nonatomic, strong) id object;
+@end
+
+@implementation ObjectAttachmentCell
+@synthesize  object;
+-(id)initWithObject: (id)obj
+{
+	self = [super init];
+	self.object = obj;
+	return self;
+}
+
+@end
+
+@interface AttachableObject : OFObject{
+	@private
+	id object;
+}
+-(id)initWithObject: (id)obj;
+@property (nonatomic, strong) id object;
+@end
+
+@implementation AttachableObject
+@synthesize object;
+-(id)initWithObject: (id)obj
+{
+	self = [super init];
+	self.object = obj;
+	return self;
+}
+
+-(id)attachmentCell
+{
+	return [[ObjectAttachmentCell alloc] initWithObject: self.object];
+}
+
+@end
+
 @implementation NSAttributedString_NTIExtensionsTest
 
+-(void)testAttributeStringFromBasicObject
+{
+	NSAttributedString* objectString = [NSAttributedString attributedStringFromObject: 
+										[[AttachableObject alloc] initWithObject: [NSNumber numberWithInt: 10]]];
+	
+	STAssertTrue([objectString length] == 1, nil);
+	STAssertEquals([objectString.string characterAtIndex: 0], (unichar)OAAttachmentCharacter, nil);
+	
+	id attachment = [objectString attribute: OAAttachmentAttributeName atIndex: 0 effectiveRange: NULL];
+	
+	id attachmentCell = [attachment attachmentCell];
+	
+	STAssertEqualObjects([attachmentCell object], [NSNumber numberWithInt: 10], nil);
+}
+
+-(void)testBasicObjectFromAttributedString
+{
+	id object = [NSNumber numberWithInt: 10];
+	
+	NSAttributedString* objectString = [NSAttributedString attributedStringFromObject: 
+										[[AttachableObject alloc] initWithObject: object]];
+	
+	NSArray* parsedObjects = [objectString objectsFromAttributedString];
+	
+	STAssertTrue([parsedObjects count] == 1, nil);
+	STAssertEqualObjects([parsedObjects firstObject], object, nil);
+	
+	
+}
+
+-(void)testObjectThenTextWithNoSeparator
+{
+	id object = [NSNumber numberWithInt: 10];
+	
+	NSAttributedString* objectString = [NSAttributedString attributedStringFromObject: 
+										[[AttachableObject alloc] initWithObject: object]];
+	
+	NSMutableAttributedString* fullString = [[NSMutableAttributedString alloc] initWithAttributedString: objectString];
+	[fullString appendAttributedString: [[NSAttributedString alloc] initWithString: @"text"]];
+	
+	NSArray* objects = [fullString objectsFromAttributedString];
+	
+	STAssertTrue([objects count] == 2, nil);
+	STAssertEqualObjects([objects firstObject], object, nil);
+	STAssertEqualObjects([objects secondObject], @"text", nil);
+	
+}
 
 -(void)testAttributedStringFromAttributedStrings
 {
@@ -30,11 +123,11 @@
 	
 	NSAttributedString* joined = [NSAttributedString attributedStringFromAttributedStrings: parts];
 	
-	STAssertTrue(joined.string.length == 13, nil);
+	STAssertTrue(joined.string.length == 12, nil);
 	
 	STAssertTrue([joined.string characterAtIndex: 1] == OAAttachmentCharacter , nil);
 	STAssertTrue([joined.string characterAtIndex: 6] == OAAttachmentCharacter , nil);
-	STAssertTrue([joined.string characterAtIndex: joined.string.length -1 ] == OAAttachmentCharacter , nil);
+	STAssertTrue([joined.string characterAtIndex: joined.string.length -1 ] != OAAttachmentCharacter , nil);
 	
 	//What kind of pizza
 	NSString* kind = [joined attribute: @"Kind" atIndex: 9 effectiveRange: NULL];
