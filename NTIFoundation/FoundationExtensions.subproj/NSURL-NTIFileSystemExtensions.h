987fd4acc5dbe3323eafd12e4a176823a8c37ebd
Chris Utz <chris.utz@nextthought.com>
2011-12-14 19:47:20 +0000

    Not sure how that even built the first time

--- NTIFoundation/FoundationExtensions.subproj/NSURL-NTIFileSystemExtensions.h
+++ NTIFoundation/FoundationExtensions.subproj/NSURL-NTIFileSystemExtensions.h
@@ -11,16 +11,7 @@
 
 @interface NSURL(NTIFileSystemExtensions)
 
-
-- (BOOL)addSkipBackupAttributeToItem
-{
-    const char* filePath = [[URL path] fileSystemRepresentation];
-	
-    const char* attrName = "com.apple.MobileBackup";
-    u_int8_t attrValue = 1;
-	
-    int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
-    return result == 0;
-}
+//Adds the skip backup xattr to the item at this url.  The file or folder should already exist.
+- (BOOL)addSkipBackupAttributeToItem;
 
 @end
