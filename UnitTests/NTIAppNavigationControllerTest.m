7421cf6fd9ae3b0c9742c8dcd26e4a2ab8d0f96d
Chris Utz <chris.utz@nextthought.com>
2012-01-31 20:38:06 +0000

    Test moving an app layer to the top of the stack.  Can't get this to run as a logic test because it manipulates view objects.  Need to find a solution for that.

--- UnitTests/NTIAppNavigationControllerTest.m
+++ UnitTests/NTIAppNavigationControllerTest.m
@@ -20,6 +20,10 @@
 @implementation TestTransLayer
 @end
 
+@interface NTIAppNavigationController(PrivateMessagesToTest)
+-(void)bringLayerForward: (id<NTIAppNavigationLayer>)layer;
+@end
+
 
 @implementation NTIAppNavigationControllerTest
 
@@ -57,5 +61,44 @@
 	[self->appNavController popLayerAnimated: NO];
 	STAssertEqualObjects([self->appNavController layers], currentLayers, nil);
 }
+
+//TODO this can't be run by logic tests.  It does view manipulation which triggers this error.  We may need to setup
+//Application test for this project using the sample app?
+// 
+// 2012-01-31 14:16:57.229 otest[61963:7803] ERROR: System image table has not been initialized. 
+// Do not ask for images or set up UI before UIApplicationMain() has been called.
+
+
+//-(void)testMovingAppLayer
+//{
+//	id appLayer1 = [[TestAppLayer alloc] initWithNibName: nil bundle: nil];
+//	id transLayer1 = [[TestTransLayer alloc] initWithNibName: nil bundle: nil];
+//	id transLayer2 = [[TestTransLayer alloc] initWithNibName: nil bundle: nil];
+//	id appLayer2 = [[TestAppLayer alloc] initWithNibName: nil bundle: nil];
+//	
+//	[self->appNavController pushLayer: appLayer1 animated: NO];
+//	[self->appNavController pushLayer: transLayer1 animated: NO];
+//	[self->appNavController pushLayer: transLayer2 animated: NO];
+//	[self->appNavController pushLayer: appLayer2 animated: NO];
+//	
+//	NSArray* layers = [NSArray arrayWithObjects: self->rootLayer, 
+//					   appLayer1, transLayer1, 
+//					   transLayer2, appLayer2, nil];
+//	
+//	STAssertEqualObjects(self->appNavController.layers, 
+//						 layers, nil);
+//	
+//	[self->appNavController bringLayerForward: appLayer1];
+//	
+//	STAssertEquals(self->appNavController.topApplicationLayer, appLayer1, nil);
+//	
+//	layers = [NSArray arrayWithObjects: self->rootLayer, 
+//			  appLayer2, appLayer1, transLayer1, 
+//			  transLayer2, nil];
+//	
+//	STAssertEqualObjects(self->appNavController.layers, 
+//						 layers, nil);
+//	
+//}
 
 @end
