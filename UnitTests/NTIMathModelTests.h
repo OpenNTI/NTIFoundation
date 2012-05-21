89f2089c1f94d8dbad63fccc9c93c0ce498a63d4
Logan Testi <logan.testi@nextthought.com>
2012-05-21 20:33:38 +0000

    The header file for the ipad toString and latexValue test for the NTIMathInputExpressionModel tests

---
+++ UnitTests/NTIMathModelTests.h
@@ -0,0 +1,19 @@
+//
+//  NTIToStringTest.h
+//  NTIFoundation
+//
+//  Created by Logan Testi on 5/21/12.
+//  Copyright (c) 2012 NextThought. All rights reserved.
+//
+//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@class NTIMathInputExpressionModel;
+@interface NTIMathModelTests : SenTestCase {
+@private
+	NTIMathInputExpressionModel* mathModel;
+}
+
+@end
