f5057c77f567ea644b37567282985b4a053a2fb1
Pacifique Mahoro <pacifique.mahoro@nextthought.com>
2012-05-30 18:48:02 +0000

    Adjust conditionally to the new 5.1 flagging system to not back up our local storage in the cloud. It's currently commented out because we are building for 5.0 and not 5.1 which supports the constant we need to check.

--- NTIFoundation/FoundationExtensions.subproj/NSURL-NTIFileSystemExtensions.m
+++ NTIFoundation/FoundationExtensions.subproj/NSURL-NTIFileSystemExtensions.m
@@ -9,16 +9,31 @@
 #import "NSURL-NTIFileSystemExtensions.h"
 
 @implementation NSURL(NTIFileSystemExtensions)
-
 - (BOOL)addSkipBackupAttributeToItem
 {
-    const char* filePath = [[self path] fileSystemRepresentation];
-	
-    const char* attrName = "com.apple.MobileBackup";
-    u_int8_t attrValue = 1;
-	
-    int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
-    return result == 0;
+	//We want to behave differently based on the version of the device, with the new "don't backup" flagging mechanism introduced in iOS 5.1.
+	//FIXME: the following code is commented out because we are currently building for 5.0 not 5.1, which supports the needed constant.
+	//5.1 or later
+//	if (&NSURLIsExcludedFromBackupKey) {
+//		assert([[NSFileManager defaultManager] fileExistsAtPath: [self path]]);
+//		
+//		NSError *error = nil;
+//		BOOL success = [self setResourceValue: [NSNumber numberWithBool: YES]
+//									  forKey: NSURLIsExcludedFromBackupKey error: &error];
+//		if(!success){
+//			NSLog(@"Error excluding %@ from backup %@", [self lastPathComponent], error);
+//		}
+//		return success;
+//	}
+//	else {
+		const char* filePath = [[self path] fileSystemRepresentation];
+		
+		const char* attrName = "com.apple.MobileBackup";
+		u_int8_t attrValue = 1;
+		
+		int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
+		return result == 0;
+//	}
 }
 
 @end
