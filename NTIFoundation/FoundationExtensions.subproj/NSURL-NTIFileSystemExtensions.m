7578fb7844064e3b32346744241e88fa8f451850
Chris Utz <chris.utz@nextthought.com>
2012-05-31 14:40:14 +0000

    Use a preprocessor macro.

--- NTIFoundation/FoundationExtensions.subproj/NSURL-NTIFileSystemExtensions.m
+++ NTIFoundation/FoundationExtensions.subproj/NSURL-NTIFileSystemExtensions.m
@@ -12,28 +12,32 @@
 - (BOOL)addSkipBackupAttributeToItem
 {
 	//We want to behave differently based on the version of the device, with the new "don't backup" flagging mechanism introduced in iOS 5.1.
-	//FIXME: the following code is commented out because we are currently building for 5.0 not 5.1, which supports the needed constant.
-	//5.1 or later
-//	if (&NSURLIsExcludedFromBackupKey) {
-//		assert([[NSFileManager defaultManager] fileExistsAtPath: [self path]]);
-//		
-//		NSError *error = nil;
-//		BOOL success = [self setResourceValue: [NSNumber numberWithBool: YES]
-//									  forKey: NSURLIsExcludedFromBackupKey error: &error];
-//		if(!success){
-//			NSLog(@"Error excluding %@ from backup %@", [self lastPathComponent], error);
-//		}
-//		return success;
-//	}
-//	else {
-		const char* filePath = [[self path] fileSystemRepresentation];
-		
-		const char* attrName = "com.apple.MobileBackup";
-		u_int8_t attrValue = 1;
-		
-		int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
-		return result == 0;
-//	}
+	
+	//Only compiles against 5.1, but no _5_1 constant defined yet.  Is this the right condition?
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_5_0 
+	
+	//If the new excluded from backup key is defined use the new way 
+	if (&NSURLIsExcludedFromBackupKey) {		
+		NSError *error = nil;
+		BOOL success = [self setResourceValue: [NSNumber numberWithBool: YES]
+									   forKey: NSURLIsExcludedFromBackupKey 
+										error: &error];
+		if(!success){
+			NSLog(@"Error excluding %@ from backup %@", self, error);
+		}
+		return success;
+	}
+#endif
+
+	//Else fallback to the old way of setting
+	const char* filePath = [[self path] fileSystemRepresentation];
+	
+	const char* attrName = "com.apple.MobileBackup";
+	u_int8_t attrValue = 1;
+	
+	int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
+	return result == 0;
+	
 }
 
 @end
