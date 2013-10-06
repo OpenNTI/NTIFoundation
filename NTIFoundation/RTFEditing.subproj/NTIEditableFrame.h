364280fd625df6492541d73e61c2ff051fb51780
cutz <christopher.m.utz@gmail.com>
2013-10-06 09:25:26 -0500

    Update comments to actually reflect the reason for subclassing.

--- NTIFoundation/RTFEditing.subproj/NTIEditableFrame.h
+++ NTIFoundation/RTFEditing.subproj/NTIEditableFrame.h
@@ -18,9 +18,7 @@
 @end
 
 /**
- * Exists to workaround some bugs manifesting in iOS 5.0beta 6+, specifically
- * OUIEditableFrame breaks badly when asked to handle writing directions. We
- * "fix" this by hardcoding Left-to-Right.
+ * Extend the OUITextView with some helpful messages (particularily around attachment handling)
  */
 @interface NTIEditableFrame : OUITextView{
 	@private
@@ -33,7 +31,3 @@
 
 @end
 
-//We also replace the base methods until such time as they are
-//fixed, because OUIEditableFrame is used in places that we cannot
-//be (deep in the inspector, for instance). Pose-as-class would make this
-//easier!
