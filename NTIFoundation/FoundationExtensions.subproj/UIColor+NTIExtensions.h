9700651e926f4c78b46eadd51eedb3f5140074ac
BryanHoke <bryan.hoke@nextthought.com>
2014-05-28 14:52:52 -0500

    De-exposes +unsignedIntegerFromHexString:. Adds documentation to +colorFromHexString:withAlpha:.

--- NTIFoundation/FoundationExtensions.subproj/UIColor+NTIExtensions.h
+++ NTIFoundation/FoundationExtensions.subproj/UIColor+NTIExtensions.h
@@ -10,9 +10,13 @@
 
 @interface UIColor (NTIExtensions)
 
+/**
+ * Creates a UIColor object by parsing a string which textually represents red, green, and blue hex-values, with alpha specified separately.
+ * @param hexString A string-encoding of red, green, and blue hexadecimal color-values. E.g., "7b8cdf". May be prefixed by "#".
+ * @param alpha Floating-point alpha (transparency) value specified in the range of 0 to 1 (1 being completely opaque, 0 being completely transparent).
+ * @return A UIColor object with red, green, and blue values as represented by |hexString| and alpha value specified by |alpha|.
+ */
 + (UIColor *)colorFromHexString: (NSString *)hexString
 					  withAlpha: (CGFloat)alpha;
-
-+ (NSUInteger)unsignedIntegerFromHexString: (NSString *)hexString;
 
 @end
