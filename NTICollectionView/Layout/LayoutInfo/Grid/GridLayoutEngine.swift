cc999e7153dd7de01b8cf5f3bd844f99c8d5d0bd
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-10 14:41:38 -0600

    Added GridLayoutEngine.

---
+++ NTICollectionView/Layout/LayoutInfo/Grid/GridLayoutEngine.swift
@@ -0,0 +1,17 @@
+//
+//  GridLayoutEngine.swift
+//  NTIFoundation
+//
+//  Created by Bryan Hoke on 3/10/16.
+//  Copyright © 2016 NextThought. All rights reserved.
+//
+
+import UIKit
+
+public class GridLayoutEngine: NSObject {
+	
+	var layoutInfo: LayoutInfo!
+	var layoutMeasure: CollectionViewLayoutMeasuring!
+	var invalidationContext: UICollectionViewLayoutInvalidationContext?
+	
+}
