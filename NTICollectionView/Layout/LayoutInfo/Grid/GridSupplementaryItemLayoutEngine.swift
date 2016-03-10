160b8a31db6f52ff6c19f840c6adae1fc1c7847a
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-10 14:43:03 -0600

    Added GridSupplementaryItemLayoutEngine.

---
+++ NTICollectionView/Layout/LayoutInfo/Grid/GridSupplementaryItemLayoutEngine.swift
@@ -0,0 +1,13 @@
+//
+//  GridSupplementaryItemLayoutEngine.swift
+//  NTIFoundation
+//
+//  Created by Bryan Hoke on 3/10/16.
+//  Copyright © 2016 NextThought. All rights reserved.
+//
+
+import UIKit
+
+class GridSupplementaryItemLayoutEngine: NSObject {
+
+}
