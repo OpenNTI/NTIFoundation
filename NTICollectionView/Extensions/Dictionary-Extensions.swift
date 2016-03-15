d985d042abd06cadaef526f2ced8fdefeb4cf8ce
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-15 14:13:16 -0500

    Added Dictionary-Extensions.

---
+++ NTICollectionView/Extensions/Dictionary-Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Dictionary-Extensions.swift
+//  NTIFoundation
+//
+//  Created by Bryan Hoke on 3/15/16.
+//  Copyright © 2016 NextThought. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary where Value: protocol<RangeReplaceableCollectionType, ArrayLiteralConvertible> {
+	
+	public mutating func append(x: Value.Generator.Element, to key: Key) {
+		var items = self[key] ?? []
+		items.append(x)
+		self[key] = items
+	}
+	
+	public mutating func appendContentsOf(newElements: Value, to key: Key) {
+		var items = self[key] ?? []
+		items.appendContentsOf(newElements)
+		self[key] = items
+	}
+	
+}
