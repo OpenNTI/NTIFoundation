468ddc0c27544995c79398f352b1b3fd0adc6d6d
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-18 18:56:54 -0500

    Update ViewControllerDelegate. Implement DelegatingViewController. Remove ViewControllerDelegate property from CollectionViewController.

--- NTICollectionView/ViewControllers/CollectionViewController.swift
+++ NTICollectionView/ViewControllers/CollectionViewController.swift
@@ -13,8 +13,6 @@ private var UpdateNumber = 0
 
 public class CollectionViewController: UICollectionViewController, CollectionDataSourceDelegate, CollectionViewSupplementaryViewTracking {
 	
-	public weak var delegate: ViewControllerDelegate?
-	
 	private var updateCompletionHandler: dispatch_block_t?
 	private let reloadedSections = NSMutableIndexSet()
 	private let deletedSections = NSMutableIndexSet()
@@ -210,13 +208,11 @@ public class CollectionViewController: UICollectionViewController, CollectionDat
 	
 	public override func willTransitionToTraitCollection(newCollection: UITraitCollection, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
 		clearCalculatedInsetInfo()
-		delegate?.viewController(self, willTransitionTo: newCollection, with: coordinator)
 		super.willTransitionToTraitCollection(newCollection, withTransitionCoordinator: coordinator)
 	}
 	
 	public override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
 		clearCalculatedInsetInfo()
-		delegate?.viewController(self, viewWillTransitionTo: size, with: coordinator)
 		super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
 	}
 	
--- NTICollectionView/ViewControllers/ViewControllerDelegate.swift
+++ NTICollectionView/ViewControllers/ViewControllerDelegate.swift
@@ -10,8 +10,24 @@ import UIKit
 
 public protocol ViewControllerDelegate: class {
 	
+	func viewControllerViewDidLoad(viewController: UIViewController)
+	
+	func viewControllerViewWillAppear(viewController: UIViewController)
+	
 	func viewController(viewController: UIViewController, willTransitionTo newCollection: UITraitCollection, with transitionCoordinator: UIViewControllerTransitionCoordinator)
 	
 	func viewController(viewController: UIViewController, viewWillTransitionTo size: CGSize, with transitionCoordinator: UIViewControllerTransitionCoordinator)
+	
+}
+
+extension ViewControllerDelegate {
+	
+	public func viewControllerViewDidLoad(viewController: UIViewController) { }
+	
+	public func viewControllerViewWillAppear(viewController: UIViewController) { }
+	
+	public func viewController(viewController: UIViewController, willTransitionTo newCollection: UITraitCollection, with transitionCoordinator: UIViewControllerTransitionCoordinator) { }
+	
+	public func viewController(viewController: UIViewController, viewWillTransitionTo size: CGSize, with transitionCoordinator: UIViewControllerTransitionCoordinator) { }
 	
 }
