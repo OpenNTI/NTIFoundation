5809fed8c01423895a91d7163206d5576be602cc
Bryan Hoke <bryan.hoke@nextthought.com>
2016-03-23 13:57:51 -0500

    Declare `supplementaryViewRegistrations`.

--- NTICollectionView/DataSource/CollectionDataSourceController.swift
+++ NTICollectionView/DataSource/CollectionDataSourceController.swift
@@ -6,13 +6,17 @@
 //  Copyright © 2016 NextThought. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public typealias SupplementaryViewRegistration = (viewClass: UICollectionReusableView.Type?, elementKind: String, identifier: String)
 
 /// A type which manages a `CollectionDataSource` instance.
 public protocol CollectionDataSourceController: class {
 	
 	/// The `CollectionDataSource` instance managed by `self`.
 	var dataSource: CollectionDataSource { get }
+	
+	var supplementaryViewRegistrations: [SupplementaryViewRegistration] { get }
 	
 	/// `SupplementaryItem`s which may be contributed to the global section -- or, if `self` is the root controller, the supplementary items which have been contributed by child controllers.
 	var contributionalGlobalSupplementaryItemsByKey: [String: SupplementaryItem] { get }
