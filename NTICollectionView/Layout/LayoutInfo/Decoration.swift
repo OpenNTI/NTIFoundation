4af4646de414248be808a5960396a62fde74349e
Bryan Hoke <bryan.hoke@nextthought.com>
2016-04-08 16:13:42 -0500

    Overhaul `LayoutDecoration`.

--- NTICollectionView/Layout/LayoutInfo/Decoration.swift
+++ NTICollectionView/Layout/LayoutInfo/Decoration.swift
@@ -14,52 +14,186 @@ public protocol DecorationProvider {
 	
 }
 
-public protocol LayoutDecoration {
-	
-	associatedtype SectionType: LayoutSection
+public protocol LayoutDecoration: DecorationAttributeProvider {
 	
 	var elementKind: String { get }
 	
-	var layoutAttributes: CollectionViewLayoutAttributes { get }
-	
 	var indexPath: NSIndexPath { get }
 	
-	var configureDecoration: ((inout attributes: CollectionViewLayoutAttributes, section: SectionType, indexPath: NSIndexPath, invalidationContext: UICollectionViewLayoutInvalidationContext?) -> Void)? { get }
+	var itemIndex: Int { get set }
 	
-	mutating func update(with section: SectionType, indexPath: NSIndexPath, invalidationContext: UICollectionViewLayoutInvalidationContext?)
+	var sectionIndex: Int { get set }
+	
+	var layoutAttributes: UICollectionViewLayoutAttributes { get }
+	
+	mutating func setContainerFrame(containerFrame: CGRect, invalidationContext: UICollectionViewLayoutInvalidationContext?)
 	
 }
 
-public struct GridLayoutDecoration<SectionType: GridLayoutSection>: LayoutDecoration {
+private let hairline: CGFloat = 1.0 / UIScreen.mainScreen().scale
+
+public let collectionElementKindHorizontalSeparator = "collectionElementKindHorizontalSeparator"
+
+public enum HorizontalSeparatorPosition {
 	
-	public init(elementKind: String, indexPath: NSIndexPath = NSIndexPath()) {
-		self.elementKind = elementKind
-		layoutAttributes = CollectionViewLayoutAttributes(forDecorationViewOfKind: elementKind, withIndexPath: indexPath)
+	/// The separator appears at the top of its container.
+	case top
+	
+	/// The separator appears at the bottom of its container.
+	case bottom
+	
+	/// The separator appears at a fixed y-coordinate relative to its container's origin.
+	case fixed(CGFloat)
+	
+	/// The separator appears at a y-coordinate proportional to a ratio of its container's height, relative to its container's origin.
+	///
+	/// The associated value should be between `0.0` and `1.0`; any other values will be clamped to this range.
+	case ratio(CGFloat)
+	
+}
+
+public struct HorizontalSeparatorDecoration: LayoutDecoration, DecorationAttributesWrapper {
+	
+	public init(position: HorizontalSeparatorPosition) {
+		self.position = position
 	}
 	
-	public let elementKind: String
+	public var thickness: CGFloat = hairline
 	
-	public private(set) var layoutAttributes: CollectionViewLayoutAttributes
+	public var leftMargin: CGFloat = 0
+	
+	public var rightMargin: CGFloat = 0
+	
+	public var position: HorizontalSeparatorPosition
+	
+	public let elementKind = collectionElementKindHorizontalSeparator
 	
 	public var indexPath: NSIndexPath {
-		return layoutAttributes.indexPath
+		return NSIndexPath(forItem: itemIndex, inSection: sectionIndex)
 	}
 	
-	public var configureDecoration: ((inout attributes: CollectionViewLayoutAttributes, section: SectionType, indexPath: NSIndexPath, invalidationContext: UICollectionViewLayoutInvalidationContext?) -> Void)?
+	public var itemIndex: Int = 0
 	
-	public mutating func update(with section: SectionType, indexPath: NSIndexPath, invalidationContext: UICollectionViewLayoutInvalidationContext?) {
-		if indexPath != layoutAttributes.indexPath {
-			layoutAttributes = CollectionViewLayoutAttributes(forDecorationViewOfKind: elementKind, withIndexPath: indexPath)
+	public var sectionIndex: Int = 0
+	
+	public var attributes = DecorationAttributes()
+	
+	public var layoutAttributes: UICollectionViewLayoutAttributes {
+		let layoutAttributes = CollectionViewLayoutAttributes(forDecorationViewOfKind: elementKind, withIndexPath: indexPath)
+		layoutAttributes.frame = frame
+		layoutAttributes.backgroundColor = color
+		layoutAttributes.zIndex = zIndex
+		layoutAttributes.hidden = isHidden
+		return layoutAttributes
+	}
+	
+	public mutating func setContainerFrame(containerFrame: CGRect, invalidationContext: UICollectionViewLayoutInvalidationContext?) {
+		frame.origin.x = containerFrame.minX + leftMargin
+		frame.origin.y = computeYCoordinate(for: containerFrame)
+		frame.size.width = containerFrame.width - leftMargin - rightMargin
+		frame.size.height = thickness
+		
+		invalidationContext?.invalidateDecorationElement(with: layoutAttributes)
+	}
+	
+	private func computeYCoordinate(for containerFrame: CGRect) -> CGFloat {
+		switch position {
+		case .top:
+			return containerFrame.minY
+		case .bottom:
+			return containerFrame.maxY
+		case let .fixed(y):
+			return containerFrame.minY + y
+		case let .ratio(r):
+			let r = min(max(r, 0), 1)
+			return containerFrame.minY + r * containerFrame.height
 		}
-		guard let configure = configureDecoration else {
-			return
-		}
-		configure(attributes: &layoutAttributes, section: section, indexPath: indexPath, invalidationContext: invalidationContext)
 	}
 	
 }
 
-public protocol DecorationAttributesProtocol {
+public let collectionElementKindVerticalSeparator = "collectionElementKindVerticalSeparator"
+
+public enum VerticalSeparatorPosition {
+	
+	/// The separator appears at the left of its container.
+	case left
+	
+	/// The separator appears at the right of its container.
+	case right
+	
+	/// The separator appears at a fixed x-coordinate relative to its container's origin.
+	case fixed(CGFloat)
+	
+	/// The separator appears at a x-coordinate proportional to a ratio of its container's width, relative to its container's origin.
+	///
+	/// The associated value should be between `0.0` and `1.0`; any other values will be clamped to this range.
+	case ratio(CGFloat)
+	
+}
+
+public struct VerticalSeparatorDecoration: LayoutDecoration, DecorationAttributesWrapper {
+	
+	public init(position: VerticalSeparatorPosition) {
+		self.position = position
+	}
+	
+	public var thickness: CGFloat = hairline
+	
+	public var topMargin: CGFloat = 0
+	
+	public var bottomMargin: CGFloat = 0
+	
+	public var position: VerticalSeparatorPosition
+	
+	public let elementKind = collectionElementKindVerticalSeparator
+	
+	public var indexPath: NSIndexPath {
+		return NSIndexPath(forItem: itemIndex, inSection: sectionIndex)
+	}
+	
+	public var itemIndex: Int = 0
+	
+	public var sectionIndex: Int = 0
+	
+	public var attributes = DecorationAttributes()
+	
+	public var layoutAttributes: UICollectionViewLayoutAttributes {
+		let layoutAttributes = CollectionViewLayoutAttributes(forDecorationViewOfKind: elementKind, withIndexPath: indexPath)
+		layoutAttributes.frame = frame
+		layoutAttributes.backgroundColor = color
+		layoutAttributes.zIndex = zIndex
+		layoutAttributes.hidden = isHidden
+		return layoutAttributes
+	}
+	
+	public mutating func setContainerFrame(containerFrame: CGRect, invalidationContext: UICollectionViewLayoutInvalidationContext?) {
+		
+		frame.origin.x = computeXCoordinate(for: containerFrame)
+		frame.origin.y = topMargin
+		frame.size.width = thickness
+		frame.size.height = containerFrame.height - topMargin - bottomMargin
+		
+		invalidationContext?.invalidateDecorationElement(with: layoutAttributes)
+	}
+	
+	private func computeXCoordinate(for containerFrame: CGRect) -> CGFloat {
+		switch position {
+		case .left:
+			return containerFrame.minX
+		case .right:
+			return containerFrame.maxX
+		case let .fixed(x):
+			return containerFrame.minX + x
+		case let .ratio(r):
+			let r = min(max(r, 0), 1)
+			return containerFrame.minX + r * containerFrame.width
+		}
+	}
+	
+}
+
+public protocol DecorationAttributeProvider {
 	
 	var frame: CGRect { get set }
 	
@@ -69,11 +203,63 @@ public protocol DecorationAttributesProtocol {
 	
 	var color: UIColor? { get set }
 	
-	var cornerRadius: CGFloat { get set }
+}
+
+public struct DecorationAttributes: DecorationAttributeProvider {
 	
-	var borderColor: UIColor? { get set }
+	public var frame: CGRect = CGRectZero
 	
-	var borderWidth: CGFloat { get set }
+	public var zIndex: Int = 0
+	
+	public var isHidden: Bool = false
+	
+	public var color: UIColor?
+	
+}
+
+public protocol DecorationAttributesWrapper: DecorationAttributeProvider {
+	
+	var attributes: DecorationAttributes { get set }
+	
+}
+
+extension DecorationAttributesWrapper {
+	
+	public var frame: CGRect {
+		get {
+			return attributes.frame
+		}
+		set {
+			attributes.frame = newValue
+		}
+	}
+	
+	public var zIndex: Int {
+		get {
+			return attributes.zIndex
+		}
+		set {
+			attributes.zIndex = newValue
+		}
+	}
+	
+	public var isHidden: Bool {
+		get {
+			return attributes.isHidden
+		}
+		set {
+			attributes.isHidden = newValue
+		}
+	}
+	
+	public var color: UIColor? {
+		get {
+			return attributes.color
+		}
+		set {
+			attributes.color = newValue
+		}
+	}
 	
 }
 
