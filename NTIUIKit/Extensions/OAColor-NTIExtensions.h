208686dd86c504f2c02a5e44938215a5c2fa534c
Chris Utz <chris.utz@nextthought.com>
2016-04-11 20:38:55 -0500

    avoid circular import by not using new macro

--- NTIUIKit/Extensions/OAColor-NTIExtensions.h
+++ NTIUIKit/Extensions/OAColor-NTIExtensions.h
@@ -7,12 +7,11 @@
 //
 
 #import "OmniAppKit/OAColor.h"
-#import "NTIUtilities.h"
 
 @interface OAColor (NTIExtensions)
 @property (readonly,nonatomic) NSString* cssString;
 @property (readonly,nonatomic) CGColorRef rgbaCGColorRef;
 
--(CGColorRef)rgbaCGColorRef NTI_METHOD_FAMILY_NEW;
+-(CGColorRef)rgbaCGColorRef __attribute__((objc_method_family(new)));
 
 @end
