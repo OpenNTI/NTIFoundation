9533a5dfc5659b273195359dffe3aac62146374a
Bryan Hoke <bryan.hoke@nextthought.com>
2016-04-12 11:06:19 -0500

    Declare `CollectionUpdateProvider` protocol with extension and have `CollectionUpdateInfo` adopt it.

--- NTICollectionView/Layout/CollectionUpdateInfo.swift
+++ NTICollectionView/Layout/CollectionUpdateInfo.swift
@@ -9,20 +9,25 @@
 import UIKit
 
 /// A container for keeping track of collection view updates.
-public struct CollectionUpdateInfo {
+public protocol CollectionUpdateProvider {
 	
-	public var insertedIndexPaths: Set<NSIndexPath> = []
-	public var removedIndexPaths: Set<NSIndexPath> = []
-	public var reloadedIndexPaths: Set<NSIndexPath> = []
+	var insertedIndexPaths: Set<NSIndexPath> { get set }
+	var removedIndexPaths: Set<NSIndexPath> { get set }
+	var reloadedIndexPaths: Set<NSIndexPath> { get set }
 	
-	public var insertedSections: Set<Int> = []
-	public var removedSections: Set<Int> = []
-	public var reloadedSections: Set<Int> = []
+	var insertedSections: Set<Int> { get set }
+	var removedSections: Set<Int> { get set }
+	var reloadedSections: Set<Int> { get set }
 	
 	/// Additional index paths to delete for given kinds of elements during updates.
-	public var additionalDeletedIndexPathsByKind: [String: [NSIndexPath]] = [:]
+	var additionalDeletedIndexPathsByKind: [String: [NSIndexPath]] { get set }
 	/// Additional index paths to insert for given kinds of elements during updates.
-	public var additionalInsertedIndexPathsByKind: [String: [NSIndexPath]] = [:]
+	var additionalInsertedIndexPathsByKind: [String: [NSIndexPath]] { get set }
+	
+	mutating func reset()
+}
+
+extension CollectionUpdateProvider {
 	
 	public mutating func reset() {
 		insertedIndexPaths = []
@@ -34,5 +39,21 @@ public struct CollectionUpdateInfo {
 		additionalDeletedIndexPathsByKind = [:]
 		additionalInsertedIndexPathsByKind = [:]
 	}
+	
+}
+
+/// A container for keeping track of collection view updates.
+public struct CollectionUpdateInfo: CollectionUpdateProvider {
+	
+	public var insertedIndexPaths: Set<NSIndexPath> = []
+	public var removedIndexPaths: Set<NSIndexPath> = []
+	public var reloadedIndexPaths: Set<NSIndexPath> = []
+	
+	public var insertedSections: Set<Int> = []
+	public var removedSections: Set<Int> = []
+	public var reloadedSections: Set<Int> = []
+	
+	public var additionalDeletedIndexPathsByKind: [String: [NSIndexPath]] = [:]
+	public var additionalInsertedIndexPathsByKind: [String: [NSIndexPath]] = [:]
 	
 }
