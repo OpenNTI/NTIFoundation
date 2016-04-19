3a1f51cfa855e3903d7396715d27e249e1442abe
Bryan Hoke <bryan.hoke@nextthought.com>
2016-04-19 17:23:09 -0500

    Declare `LayoutBuilder` and `LayoutSectionBuilder`.

---
+++ NTICollectionView/Layout/LayoutInfo/LayoutBuilder.swift
@@ -0,0 +1,23 @@
+//
+//  LayoutBuilder.swift
+//  NTIFoundation
+//
+//  Created by Bryan Hoke on 4/19/16.
+//  Copyright © 2016 NextThought. All rights reserved.
+//
+
+import UIKit
+
+/// Conforming types can build `LayoutInfo` instances from `LayoutDescription`s.
+public protocol LayoutBuilder {
+	
+	func makeLayoutInfo(using description: LayoutDescription, at origin: CGPoint) -> LayoutInfo
+	
+}
+
+/// Conforming types can build `LayoutSection` instances from `SectionDescription`s.
+public protocol LayoutSectionBuilder {
+	
+	func makeLayoutSection(using description: SectionDescription, at origin: CGPoint) -> LayoutSection
+	
+}
