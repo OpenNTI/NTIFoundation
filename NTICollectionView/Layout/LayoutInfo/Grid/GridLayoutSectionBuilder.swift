e8c3e7155455a3c50a8be2403b3c60bf2398209a
Bryan Hoke <bryan.hoke@nextthought.com>
2016-04-19 17:27:24 -0500

    Add `GridLayoutSectionBuilder`.

---
+++ NTICollectionView/Layout/LayoutInfo/Grid/GridLayoutSectionBuilder.swift
@@ -0,0 +1,21 @@
+//
+//  GridLayoutSectionBuilder.swift
+//  NTIFoundation
+//
+//  Created by Bryan Hoke on 4/19/16.
+//  Copyright © 2016 NextThought. All rights reserved.
+//
+
+import UIKit
+
+public struct GridLayoutSectionBuilder: LayoutSectionBuilder {
+	
+	public func makeLayoutSection(using description: SectionDescription, at origin: CGPoint) -> LayoutSection {
+		var section = BasicGridLayoutSection()
+		
+		// TODO: Implement
+		
+		return section
+	}
+	
+}
