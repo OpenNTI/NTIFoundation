04ee0548163edd29b8628f115e7cca06d83756e5
Bryan Hoke <bryan.hoke@nextthought.com>
2016-04-19 17:17:22 -0500

    Implement `LayoutDescription` and `SectionDescription`.

---
+++ NTICollectionView/Layout/LayoutInfo/LayoutDescription.swift
@@ -0,0 +1,37 @@
+//
+//  LayoutDescription.swift
+//  NTIFoundation
+//
+//  Created by Bryan Hoke on 4/19/16.
+//  Copyright © 2016 NextThought. All rights reserved.
+//
+
+import UIKit
+
+/// Data used for creating layout info.
+public struct LayoutDescription {
+	
+	public var size = CGSizeZero
+	
+	public var contentOffset = CGPointZero
+	
+	public var contentInset = UIEdgeInsetsZero
+	
+	public var bounds = CGRectZero
+	
+	public var sections: [SectionDescription] = []
+	
+	public var globalSection: SectionDescription?
+	
+}
+
+/// Data used for creating a layout section.
+public struct SectionDescription {
+	
+	public var sectionIndex = NSNotFound
+	
+	public var numberOfItems = 0
+	
+	public var metrics: DataSourceSectionMetrics
+	
+}
