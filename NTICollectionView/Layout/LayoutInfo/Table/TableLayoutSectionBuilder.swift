2f9264c2cb5bf7157a86bddf406141e72415466e
Bryan Hoke <bryan.hoke@nextthought.com>
2016-04-19 21:32:04 -0500

    Add `TableLayoutSectionBuilder`.

---
+++ NTICollectionView/Layout/LayoutInfo/Table/TableLayoutSectionBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  TableLayoutSectionBuilder.swift
+//  NTIFoundation
+//
+//  Created by Bryan Hoke on 4/19/16.
+//  Copyright © 2016 NextThought. All rights reserved.
+//
+
+import UIKit
+
+public struct TableLayoutSectionBuilder: LayoutSectionBuilder {
+	
+	public func makeLayoutSection(using description: SectionDescription, at origin: CGPoint) -> LayoutSection {
+		// TODO: Implement
+		preconditionFailure()
+	}
+	
+}
