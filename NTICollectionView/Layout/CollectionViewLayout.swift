0ed6bc09a2b83f683cbab8a5e4ba0046c523983b
Bryan Hoke <bryan.hoke@nextthought.com>
2016-04-22 13:38:03 -0500

    Remove unused value.

--- NTICollectionView/Layout/CollectionViewLayout.swift
+++ NTICollectionView/Layout/CollectionViewLayout.swift
@@ -219,8 +219,6 @@ public class CollectionViewLayout: UICollectionViewLayout, CollectionViewLayoutM
 				return
 		}
 		
-		let sectionIndex = indexPath.section
-		
 		guard let layoutInfo = self.layoutInfo else {
 			return
 		}
