90ab19fedb4c75680d6872d840f1f6b1ee8733d4
Bryan Hoke <bryan.hoke@nextthought.com>
2016-06-13 12:11:57 -0500

    Replace the declaration of `makeLayoutData(using:at:)` with `buildLayout(from:at:using:)` in `LayoutBuilder`.

--- NTICollectionView/Layout/LayoutInfo/LayoutBuilder.swift
+++ NTICollectionView/Layout/LayoutInfo/LayoutBuilder.swift
@@ -11,7 +11,7 @@ import UIKit
 /// Conforming types can build `LayoutInfo` instances from `LayoutDescription`s.
 public protocol LayoutBuilder {
 	
-	func makeLayoutData(using description: LayoutDescription, at origin: CGPoint) -> LayoutData
+	func buildLayout(from description: LayoutDescription, at origin: CGPoint, inout using data: LayoutData)
 	
 }
 
