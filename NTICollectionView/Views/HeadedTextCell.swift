1fc26c7453615cbca0b52484b4bb9b171e6094b5
Bryan Hoke <bryan.hoke@nextthought.com>
2016-06-16 13:34:23 -0500

    Refactor a new `HeadedTextView` out of `HeadedTextCell`.

--- NTICollectionView/Views/HeadedTextCell.swift
+++ NTICollectionView/Views/HeadedTextCell.swift
@@ -8,23 +8,13 @@
 
 import UIKit
 
-/// A `CollectionViewCell` which displays header text above primary text.
+// MARK: - HeadedTextCell
+
+/// A `CollectionViewCell` which displays a `HeadedTextView`.
 public class HeadedTextCell: CollectionViewCell {
 	
-	/// Displays the header text.
-	public let headerLabel = UILabel()
-	
-	/// Displays the primary text.
-	public let textLabel = UILabel()
-	
-	/// The vertical spacing between the header and the text.
-	public var spacing: CGFloat = 7 {
-		didSet { spacingConstraint.constant = spacing }
-	}
-	
-	private var spacingConstraint: NSLayoutConstraint!
-	
-	private let wrapper = UIView()
+	/// The `HeadedTextView` displayed by `self`.
+	public let headedTextView = HeadedTextView()
 	
 	public override init(frame: CGRect) {
 		super.init(frame: frame)
@@ -36,31 +26,69 @@ public class HeadedTextCell: CollectionViewCell {
 	}
 	
 	private func commonInit() {
-		wrapper.translatesAutoresizingMaskIntoConstraints = false
-		contentView.addSubview(wrapper)
+		headedTextView.translatesAutoresizingMaskIntoConstraints = false
+		contentView.addSubview(headedTextView)
 		
-		headerLabel.translatesAutoresizingMaskIntoConstraints = false
-		wrapper.addSubview(headerLabel)
+		let views = ["headedText": headedTextView]
+		let metrics: [String: NSNumber] = [:]
 		
-		textLabel.translatesAutoresizingMaskIntoConstraints = false
-		wrapper.addSubview(textLabel)
+		for dimension in ["H", "V"] {
+			NSLayoutConstraint.activateConstraints(NSLayoutConstraint.constraintsWithVisualFormat("\(dimension):|-[headedText]-|", options: [], metrics: metrics, views: views))
+		}
+	}
+
+}
+
+// MARK: - HeadedTextView
+
+/// A `UIView` which displays header text above primary text.
+public class HeadedTextView : UIView {
+	
+	/// Displays the header text.
+	public let headerLabel = UILabel()
+	
+	/// Displays the primary text.
+	public let textLabel = UILabel()
+	
+	/// The vertical spacing between the header and the primary text.
+	public var spacing: CGFloat = 7 {
+		didSet { spacingConstraint.constant = spacing }
+	}
+	
+	private var spacingConstraint: NSLayoutConstraint!
+	
+	public override init(frame: CGRect) {
+		super.init(frame: frame)
+		commonInit()
+	}
+	
+	public required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	private func commonInit() {
+		for view in [headerLabel, textLabel] {
+			view.translatesAutoresizingMaskIntoConstraints = false
+			addSubview(view)
+		}
 		
-		let views = ["wrapper": wrapper, "header": headerLabel, "text": textLabel]
-		let metrics = ["s": spacing]
+		let views = ["header": headerLabel, "text": textLabel]
+		let metrics: [String: NSNumber] = [:]
 		
 		var constraints = [NSLayoutConstraint]()
 		
-		constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|[header]", options: [], metrics: metrics, views: views)
-		constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[text]|", options: [], metrics: metrics, views: views)
+		constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-[header]", options: [], metrics: metrics, views: views)
+		constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[text]-|", options: [], metrics: metrics, views: views)
 		
 		spacingConstraint = NSLayoutConstraint(item: textLabel, attribute: .Top, relatedBy: .Equal, toItem: headerLabel, attribute: .Bottom, multiplier: 1, constant: spacing)
 		constraints.append(spacingConstraint)
 		
-		constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[header]|", options: [], metrics: metrics, views: views)
-		constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[text]|", options: [], metrics: metrics, views: views)
+		constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-[header]-|", options: [], metrics: metrics, views: views)
+		constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-[text]-|", options: [], metrics: metrics, views: views)
 		
-		constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-(>=0)-[wrapper]-(>=0)-|", options: [], metrics: metrics, views: views)
-		constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-(>=0)-[wrapper]-(>=0)-|", options: [], metrics: metrics, views: views)
+		NSLayoutConstraint.activateConstraints(constraints)
+	}
+	
 		
 		constraints.append(NSLayoutConstraint(item: wrapper, attribute: .CenterX, relatedBy: .Equal, toItem: contentView, attribute: .CenterX, multiplier: 1, constant: 0))
 		constraints.append(NSLayoutConstraint(item: wrapper, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0))
