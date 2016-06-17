b71e8d76d565fee6d25d05d380f1196ca8f3fc08
Bryan Hoke <bryan.hoke@nextthought.com>
2016-06-17 11:57:15 -0500

    Declare `frameInitializable`.

--- NTICollectionView/Utilities/HelperTypes.swift
+++ NTICollectionView/Utilities/HelperTypes.swift
@@ -6,12 +6,19 @@
 //  Copyright © 2016 NextThought. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 public enum Result<T> {
 	
 	case success(T)
 	
 	case failure(NSError)
+	
+}
+
+/// Conforming types can be initialized with a single `CGRect` parameter called `frame`.
+public protocol FrameInitializable {
+	
+	init(frame: CGRect)
 	
 }
